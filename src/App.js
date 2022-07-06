6bd61b6eea9d66fddddbf5e0996e2d6fe322632e
Raquel Roman-Rodriguez <rromanrodriguez@paperlesspost.com>
2022-07-06 18:06:49 -0400

    complete assignment 1-1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
+const todoList = [
+    {id: 1, title: 'finish this'},
+    { id: 2, title: 'eat dinner' },
+    { id: 3, title: 'wash dishes' },
+]
+
 function App() {
     return (
-        <div style={{ textAlign: 'center' }}>
-            
-        </div>
+        <>
+            <h1>Todo List</h1>
+            <ul>
+                {todoList.map(t => <li key={t.id}>{t.title}</li>)}
+            </ul>
+        </>
     );
 }
 
